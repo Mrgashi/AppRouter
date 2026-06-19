831f05ac0ecffec09c4e7606252cca6143e5e749
Mr.Gashi <mirdon_g@hotmail.com>
2026-06-19 02:06:30 +0200

    remove .env.example

--- nextjs-dashboard/.next/dev/server/middleware-build-manifest.js
+++ nextjs-dashboard/.next/dev/server/middleware-build-manifest.js
@@ -3,22 +3,9 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [
-    "static/chunks/55baa_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_639c0ad3._.js",
-    "static/chunks/55baa_next_dist_compiled_react-dom_303ed9a4._.js",
-    "static/chunks/55baa_next_dist_compiled_react-server-dom-turbopack_b17dc3b1._.js",
-    "static/chunks/55baa_next_dist_compiled_next-devtools_index_77111bcf.js",
-    "static/chunks/55baa_next_dist_compiled_63c30804._.js",
-    "static/chunks/55baa_next_dist_client_9273dfbb._.js",
-    "static/chunks/55baa_next_dist_53f71582._.js",
-    "static/chunks/8c7dd_@swc_helpers_cjs_bd931bb0._.js",
-    "static/chunks/nextjs-dashboard_a0ff3932._.js",
-    "static/chunks/turbopack-nextjs-dashboard_5d6b951e._.js"
-  ]
+  "rootMainFiles": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
